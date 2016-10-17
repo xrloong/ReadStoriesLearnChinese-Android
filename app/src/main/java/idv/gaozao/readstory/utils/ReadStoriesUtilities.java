45ec0990c98c52d3ed0337f90e927c6967e6a673
Xanadu Wang <xanaduw@synology.com>
2016-10-17 17:33:54 +0800

    Change to use OkHttp3.

--- app/src/main/java/idv/gaozao/readstory/utils/ReadStoriesUtilities.java
+++ app/src/main/java/idv/gaozao/readstory/utils/ReadStoriesUtilities.java
@@ -1,14 +1,19 @@
 package idv.gaozao.readstory.utils;
 
 import java.io.IOException;
+import java.security.cert.CertificateException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ReadStoriesUtilities {
 	private static final String HOST = "read-stories.appspot.com";
@@ -25,18 +30,66 @@ public class ReadStoriesUtilities {
 	}
 
 	public static String getOnlinePage(String url) {
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-		String retSrc = "";
+		OkHttpClient client = getUnsafeOkHttpClient();
+
+		if(client == null) {
+			client = new OkHttpClient();
+		}
+
+		Request request = new Request.Builder()
+				.url(url)
+				.get()
+				.build();
+		Call call = client.newCall(request);
 		try {
-			HttpResponse response = client.execute(request);
-			retSrc = EntityUtils.toString(response.getEntity());
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
+			Response response = call.execute();
+			String s = response.body().string();
+			return s;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return "";
+	}
 
-		return retSrc;
+	private static OkHttpClient getUnsafeOkHttpClient() {
+		try {
+			// Create a trust manager that does not validate certificate chains
+			final TrustManager[] trustAllCerts = new TrustManager[] {
+					new X509TrustManager() {
+						@Override
+						public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+						}
+
+						@Override
+						public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+						}
+
+						@Override
+						public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+							return new java.security.cert.X509Certificate[]{};
+						}
+					}
+			};
+
+			// Install the all-trusting trust manager
+			final SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+			// Create an ssl socket factory with our all-trusting manager
+			final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+			OkHttpClient.Builder builder = new OkHttpClient.Builder();
+			builder.sslSocketFactory(sslSocketFactory);
+			builder.hostnameVerifier(new HostnameVerifier() {
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+
+			OkHttpClient okHttpClient = builder.build();
+			return okHttpClient;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
